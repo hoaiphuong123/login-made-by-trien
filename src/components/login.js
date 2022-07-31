3a5a00ea71dbafb44bdc32fa342927fa07ea51bb
hoaiphuong123 <hp34nam@gmail.com>
2022-07-31 14:22:59 +0700

    login

--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './myname1.css';
 import PropTypes from 'prop-types';
 
 function Login({ name, password }) {
+    const [user, setUser] = useState();
+    const [pass, setPass] = useState();
+
     console.log(name);
     console.log(password);
     return (
@@ -15,10 +18,15 @@ function Login({ name, password }) {
                 </div>
                 <div className="input-container">
                     <text>Password </text>
-                    <input type="password" name={password} required />
+                    <input
+                        type="password"
+                        name={password}
+                        onChange={(e) => setUser(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="button-container">
-                    <input type="submit" />
+                    <input type="submit" onChange={(e) => setPass(e.target.value)} />
                 </div>
             </form>
         </div>
