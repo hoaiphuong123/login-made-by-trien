2c3841cc0e53ea0de1d65bf7ab26665386e02093
hoaiphuong123 <hp34nam@gmail.com>
2022-08-02 18:31:34 +0700

    login3

--- src/components/login.js
+++ src/components/login.js
@@ -2,29 +2,29 @@ import React, { useState } from 'react';
 import './myname1.css';
 import PropTypes from 'prop-types';
 
-function Login({ name, password }) {
-    const [user, setUser] = useState('');
-    const changeUser = (e) => {
-        setUser(e.target.value);
-    };
-    const [passUser, setPassUser] = useState('');
-    const changePass = (e) => {
-        setPassUser(e.target.value);
-    };
-    const [pass, setPass] = React.useState(false);
+function Login({ name, password, onSetName, onSetPass }) {
+    // const [user, setUser] = useState('');
+    // const changeUser = (e) => {
+    //     setUser(e.target.value);
+    // };
+    // const [passUser, setPassUser] = useState('');
+    // const changePass = (e) => {
+    //     setPassUser(e.target.value);
+    // };
+    const [pass, setPass] = useState(false);
     const handleClick = () => {
         setPass(!pass);
     };
-    const submitHandler = (e) => {
-        e.preventDefault();
-        console.log({ user });
-        console.log({ passUser });
-    };
+    // const submitHandler = (e) => {
+    //     e.preventDefault();
+    //     console.log({ user });
+    //     console.log({ passUser });
+    // };
     return (
         <div className="fromlogin">
             <div className="input-container">
                 <lable htmlFor="user">Username</lable>
-                <input id="user" type="text" name={name} onChange={changeUser} required />
+                <input id="user" type="text" name={name} onChange={onSetName} required />
             </div>
             <div className="input-container">
                 <lable htmlFor="passUser">Password</lable>
@@ -32,9 +32,11 @@ function Login({ name, password }) {
                     id="passUser"
                     name={password}
                     type={pass ? 'text' : 'password'}
-                    onChange={changePass}
+                    onChange={onSetPass}
                     required
                 />
+            </div>
+            <div className="checkbox">
                 <input id="checkbox" type="checkbox" checked={pass} onChange={handleClick} />
             </div>
         </div>
@@ -43,6 +45,8 @@ function Login({ name, password }) {
 
 Login.propTypes = {
     name: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired
+    password: PropTypes.string.isRequired,
+    onSetName: PropTypes.func.isRequired,
+    onSetPass: PropTypes.func.isRequired
 };
 export default Login;
--- src/components/user.js
+++ src/components/user.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './login';
 
 function Loginuser() {
-    const changeSubmitHandler = (text) => {
-        console.log(name);
-        console.log(password);
+    const [name, setName] = useState('');
+    const [password, setPass] = useState('');
+    const handleName = (e) => {
+        setName(e.target.value);
+    };
+    const handlePass = (e) => {
+        setPass(e.target.value);
+    };
+    const changeHandleName = (e) => {
+        e.preventDefault();
+        console.log({ name });
+        console.log({ password });
     };
     return (
-        <div>
-            <form onSubmit={submitHandler}>
+        <div className="container">
+            <form onSubmit={changeHandleName}>
                 <text>Login</text>
-                <Login name="admin" password="123" />
+                <Login name="admin" password="123" onSetName={handleName} onSetPass={handlePass} />
                 <div className="button-container">
                     <input type="submit" value="submit" />
                 </div>
